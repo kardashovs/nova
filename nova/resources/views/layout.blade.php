13a26406e936a19b7305c95d657489d1aa840e42
jltkd <jl@turnkeydigital.com>
2018-09-13 09:23:25 -0500

    Updated Nova to 1.0.16

--- nova/resources/views/layout.blade.php
+++ nova/resources/views/layout.blade.php
@@ -54,7 +54,9 @@
                     @yield('content')
 
                     <p class="mt-8 text-center text-xs text-80">
-                        &copy; {{ date('Y') }} TurnKey Digital
+                        <a href="http://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
+                        <span class="px-1">&middot;</span>
+                        &copy; {{ date('Y') }} Laravel LLC - By Taylor Otwell, David Hemphill, and Steve Schoger.
                         <span class="px-1">&middot;</span>
                         v{{ Laravel\Nova\Nova::version() }}
                     </p>
